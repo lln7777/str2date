1347bb1113d31d10656c5d67c7aeca2d457e2891
Ansel Chen <chenang@fosun.com>
2020-09-08 16:20:12 +0800

    Init

---
+++ README.md
@@ -0,0 +1,54 @@
+## 字符转时间
+
+本模块主要面向爬虫和Excel解析等程序中时间字符的识别，可以将大部分常见的时间字符串转化为JS的Date类型。
+源码使用的是低级语法，兼容CommonJS、AMD、CMD，可在Node 所有版本、和大部分浏览器中使用。
+核心用法（以ES6语法示例）：
+
+#### CommonJS
+
+```javascript
+const str2date = require('parsetime-zhcn');
+const date = trans('2013年01月13日16:30:18');
+console.log(date);
+```
+
+#### ES6
+
+```javascript
+import str2date from 'parsetime-zhcn';
+const date = trans('2013年01月13日16:30:18');
+console.log(date);
+```
+
+### 示例
+
+```javascript
+const str2date = require('parsetime-zhcn');
+
+console.log(str2date('01月13号8:30am').toLocaleString())
+console.log(str2date('01月13号8:30pm').toLocaleString())
+console.log(str2date('01月13号早上8:30').toLocaleString())
+console.log(str2date('01月13号下午16:30').toLocaleString())
+console.log(str2date('2013年01月13日16:30:18').toLocaleString())
+console.log(str2date('2018-01-13 16:29:06').toLocaleString())
+console.log(str2date('2018/01/13 16:29:06').toLocaleString())
+console.log(str2date('2018.1.13 16:29:06').toLocaleString())
+console.log(str2date('1/13/2018 16:29:06').toLocaleString())
+console.log(str2date('10:10:10').toLocaleString())
+console.log(str2date('10：10').toLocaleString())
+console.log(str2date('10：10 pm').toLocaleString())
+console.log(str2date('下午10：10').toLocaleString())
+console.log(str2date('上午10：10 pm').toLocaleString())
+console.log(str2date('am 10：10').toLocaleString())
+console.log(str2date('pm 10：10').toLocaleString())
+console.log(str2date('1秒钟前').toLocaleString())
+console.log(str2date('1分钟前').toLocaleString())
+console.log(str2date('16天前').toLocaleString())
+console.log(str2date('1个月前').toLocaleString())
+console.log(str2date('1年前').toLocaleString())
+console.log(str2date('刚刚').toLocaleString())
+```
+
+## 时间转字符
+
+推荐 moment.js
---
+++ index.js
@@ -0,0 +1,95 @@
+;(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define('str2date', factory) :
+  (global = global || self, (function () {
+    var current = global.str2date;
+    var exports = global.str2date = factory();
+    exports.noConflict = function () { global.str2date = current; return exports; };
+  }()));
+}(this, (function () {
+  var timesDict = {
+    '秒': 1000,
+    '分': 1000 * 60,
+    '小': 1000 * 60 * 60,
+    '天': 1000 * 60 * 60 * 24,
+    '星': 1000 * 60 * 60 * 24 * 7,
+    '周': 1000 * 60 * 60 * 24 * 7
+  }
+  
+  function str2date(str) {
+    var datetime, timeString;
+    var now = new Date()
+    var yyyy = now.getFullYear()
+    var MM = now.getMonth() + 1
+    var dd = now.getDate()
+    var hh = now.getHours()
+    var mm = now.getMinutes()
+    var ss = now.getSeconds()
+  
+  
+    var beforeReg = /(\d+)\s*[^\s]{0,1}(年|个月|月|周|星期|天|小时|分|分钟|秒|秒钟)前/;
+    var justNowReg = /刚刚/;
+    var beforeMatches = str.match(beforeReg);
+    if (beforeMatches) {
+      var n = beforeMatches[1];
+      var unit = beforeMatches[2];
+      if (timesDict[unit[0]]) {
+        datetime = new Date(Date.now() - n * timesDict[unit[0]]);
+      } else {
+        if (unit == '月' || unit == '个月') {
+          var _y = Math.floor(n / 12);
+          var _m = n % 12;
+          yyyy -= _y;
+          MM -= _m;
+          if (MM < 1) {
+            yyyy -= 1;
+            MM += 12;
+          }
+        } else if (unit == '年') {
+          yyyy -= n;
+        } else {
+          throw new Error('Parse Datetime String Error.');
+        }
+        timeString = yyyy + '-' + MM + '-' + dd + ' ' + hh + ':' + mm + ':' + ss;
+        datetime = new Date(timeString);
+      }
+    } else if (justNowReg.test(str)) {
+      datetime = new Date();
+    } else {
+      datetime = new Date(str);
+      if (isNaN(datetime)) {
+        // 获取具体的日期
+        var zhcnDtReg = /(?:(\d{4})\s*年\s*)?(\d+)\s*月\s*(\d+)\s*(?:日|号)/i;
+        var zhcnDtRegMatch = str.match(zhcnDtReg);
+  
+        if (zhcnDtRegMatch) {
+          if (zhcnDtRegMatch[3] !== undefined) dd = +zhcnDtRegMatch[3]
+          if (zhcnDtRegMatch[2] !== undefined) MM = +zhcnDtRegMatch[2]
+          if (zhcnDtRegMatch[1] !== undefined) yyyy = +zhcnDtRegMatch[1]
+        }
+  
+        // 获取具体的时间分
+        var timeReg = /(am|pm|早上|下午|凌晨|晚上)?\s*(\d+)(?::|：)(\d+)(?:(?::|：)(\d+))?\s*(am|pm|早上|下午|凌晨|晚上)?/i;
+        var timeRegMatch = str.match(timeReg);
+  
+        if (timeRegMatch) {
+          hh = +timeRegMatch[2];
+          mm = +timeRegMatch[3];
+          ss = timeRegMatch[4] ? timeRegMatch[4] : 0;
+          var pre = timeRegMatch[1] || timeRegMatch[5];
+          if (pre && ~['pm', '下午', '晚上'].indexOf(pre.toLowerCase()) && hh < 12) {
+            hh += 12;
+          }
+        }
+        timeString = yyyy + '-' + MM + '-' + dd + ' ' + hh + ':' + mm + ':' + ss;
+        datetime = new Date(timeString);
+        if (isNaN(datetime)) {
+          datetime = null;
+        }
+      }
+    }
+    return datetime;
+  }
+  str2date.str2date = str2date;
+  return str2date
+})));
